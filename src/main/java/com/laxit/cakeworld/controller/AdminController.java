8e96bbbf096b5e0ca96a6beb6110527769536afa
laxit <laxityadav123@gmail.com>
2021-11-18 11:18:45 +0530

    Admin panel completed.

--- src/main/java/com/laxit/cakeworld/controller/AdminController.java
+++ src/main/java/com/laxit/cakeworld/controller/AdminController.java
@@ -65,6 +65,7 @@ public class AdminController {
         }
     }
 
+    //Product Section
     @GetMapping("/admin/products")
     public String getAllProducts(Model model) {
         model.addAttribute("products", productService.getAllProduct());
@@ -100,5 +101,28 @@ public class AdminController {
         product.setImageName(imageUUID);
         productService.addProduct(product);
         return "redirect:/admin/products";
+    }
+
+    @GetMapping("/admin/product/delete/{id}")
+    public String deleteProduct(@PathVariable long id) {
+        productService.removeProductById(id);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/admin/product/update/{id}")
+    public String updateProduct(@PathVariable long id, Model model) {
+        Product product = productService.getProductById(id).get();
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(product.getId());
+        productDTO.setName(product.getName());
+        productDTO.setCategoryId(product.getCategory().getId());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setWeight(product.getWeight());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setImageName(product.getImageName());
+
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("productDTO", productDTO);
+        return "productsAdd";
     }
 }
